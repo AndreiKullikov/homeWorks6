371010445be2f27b5aee0cdc3fbe3cb6605c3120
andrei <kkulmail@yandex.ru>
2023-08-23 21:44:28 +0200

    new commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+// Console.Write("Введите количество строк");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int count =0;
+// int [] strs = new int[n];
+// for (int i = 0; i < n; i++)
+// {
+//     Console.Write("Введите строку ", i + 1);
+//     strs[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// for (int i = 0; i < n; i++)
+// {
+//     Console.WriteLine(strs[i]);
+// }
+
+// for (int z = 0; z < strs.Length; z++)
+// {
+//     if (strs[z] > 0)
+//         {
+//            count++;
+//         }
+    
+// }
+// Console.Write("колличество значений больше 0= "+count);
